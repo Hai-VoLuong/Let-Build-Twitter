78dc012b09fa99e858872d1dfa9ed6c21f9a3d85
MAC <mac@Air-cua-MAC.net.fpt>
2018-01-14 00:37:17 +0700

    custom cell Collection View

--- Twitter/Twitter/HomeController.swift
+++ Twitter/Twitter/HomeController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
+class WordCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class HomeController: UICollectionViewController {
     let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .green
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -21,7 +32,6 @@ class HomeController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .blue
         return cell
     }
 }
