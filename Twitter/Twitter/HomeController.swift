00bd5caa347fcec00ab4c29bef120c95db2cf37b
MAC <mac@Air-cua-MAC.net.fpt>
2018-01-14 00:15:15 +0700

    Create HomeViewController

---
+++ Twitter/Twitter/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  Twitter
+//
+//  Created by MAC on 1/14/18.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+}
