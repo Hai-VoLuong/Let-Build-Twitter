2b86cd315c7dbe713bfdc0fb18eb25cb0a33fe09
MAC <mac@Air-cua-MAC.net.fpt>
2018-01-14 10:53:35 +0700

    Create headerView Collection

--- Twitter/Twitter/HomeController.swift
+++ Twitter/Twitter/HomeController.swift
@@ -10,7 +10,7 @@ import UIKit
 class WordCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        setupViews()
     }
     
     let wordLabel: UILabel = {
@@ -20,7 +20,7 @@ class WordCell: UICollectionViewCell {
         return label
     }()
     
-    func setupView() {
+    func setupViews() {
         backgroundColor = .yellow
         addSubview(wordLabel)
         wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
@@ -35,11 +35,14 @@ class WordCell: UICollectionViewCell {
 }
 
 class HomeController: UICollectionViewController {
+    
     let cellId = "cellId"
+    let headerId = "headerId"
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .white
         collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -49,6 +52,16 @@ class HomeController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        header.backgroundColor = .blue
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
 
