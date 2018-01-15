1395de53f948dae1f8d20859cc37035a623f00d4
MAC <mac@MacBook-Air-cua-MAC.local>
2018-01-15 22:15:09 +0700

    update project flow MVC

---
+++ Twitter/Twitter/Controllers/HomeDataSourceController.swift
@@ -0,0 +1,25 @@
+//
+//  DataSourceController.swift
+//  Twitter
+//
+//  Created by MAC on 1/14/18.
+//
+
+import LBTAComponents
+
+class HomeDataSourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDataSource = HomeDataSource()
+        self.datasource = homeDataSource
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
