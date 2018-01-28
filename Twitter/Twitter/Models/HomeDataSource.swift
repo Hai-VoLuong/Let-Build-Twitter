049ca96cd118fa7fd8179b13dd29d13c985f5c5e
MAC <mac@Air-cua-MAC.net.fpt>
2018-01-28 23:46:52 +0700

    extension array

--- Twitter/Twitter/Models/HomeDataSource.swift
+++ Twitter/Twitter/Models/HomeDataSource.swift
@@ -9,6 +9,12 @@ import LBTAComponents
 import TRON
 import SwiftyJSON
 
+extension Collection where Iterator.Element == JSON {
+    func decode() -> [Any] {
+        return []
+    }
+}
+
 class HomeDataSource: Datasource, JSONDecodable {
     
     let users: [User]
@@ -20,8 +26,11 @@ class HomeDataSource: Datasource, JSONDecodable {
             throw NSError(domain: "com.letsbuildthatapp", code: 1, userInfo: [NSLocalizedDescriptionKey: "Parsing JSON was not valid."])
         }
         
-        self.users = userJsonArray.map({User(json: $0) })
-        self.tweets = tweetsJsonArray.map({Tweet(json: $0) })
+//        self.users = userJsonArray.map({User(json: $0) })
+//        self.tweets = tweetsJsonArray.map({Tweet(json: $0) })
+        
+        self.users = userJsonArray.decode() as! [User]
+        self.tweets = tweetsJsonArray.decode() as! [Tweet]
     }
     
 
